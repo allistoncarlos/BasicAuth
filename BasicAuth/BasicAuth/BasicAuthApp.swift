6c83704b2c0e46954e41ebb3430e35258c5183dc
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2025-10-24 10:05:29 -0300

    Project Creation

---
+++ BasicAuth/BasicAuth/BasicAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAuthApp.swift
+//  BasicAuth
+//
+//  Created by Alliston Aleixo on 24/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BasicAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicAuth/BasicAuth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasicAuth
+//
+//  Created by Alliston Aleixo on 24/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
